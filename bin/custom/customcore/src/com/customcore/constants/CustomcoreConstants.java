ad625bcbbbd19cfcbbac3cb05ec67b99f52127b0
Doğuş Kar <doguskar98@gmail.com>
2023-10-25 11:30:42 +0300

    customcore
    yempty extension

---
+++ bin/custom/customcore/src/com/customcore/constants/CustomcoreConstants.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2021 SAP SE or an SAP affiliate company. All rights reserved.
+ */
+package com.customcore.constants;
+
+/**
+ * Global class for all Customcore constants. You can add global constants for your extension into this class.
+ */
+public final class CustomcoreConstants extends GeneratedCustomcoreConstants
+{
+	public static final String EXTENSIONNAME = "customcore";
+
+	private CustomcoreConstants()
+	{
+		//empty to avoid instantiating this constant class
+	}
+
+	// implement here constants used by this extension
+
+	public static final String PLATFORM_LOGO_CODE = "customcorePlatformLogo";
+}
